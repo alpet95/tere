1a05392227e195693b97a62ea04949905cc20fa2
Alex Petrov <alexpetrov0895@gmail.com>
2023-06-16 12:54:42 +0100

    feat: create global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div>Home Page</div>
-  )
+    <div>
+      <p className="regular">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+      <p className="medium">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+      <p className="semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+      <p className="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+      <p className="extrabold">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+      <p className="black">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+    </div>
+  );
 }
